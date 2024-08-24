a965fd1924f80c9e917655e976e98bc8f09e6d3a
jaquayveontavious1 <kanyingitiffany@gmail.com>
2024-08-24 14:00:22 +0300

    stored django secret key

--- backendBus/settings.py
+++ backendBus/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o)t-w_kl+jv)#=c5x#sw75dm757*klu=rbt6ss!y*c9!t)n88+'
+import os
+
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
